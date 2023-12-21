d07ca2c912dd4ff6799cf3adc66592cfa5507071
Marielbamar21 <marielbamaldonado20@gmail.com>
2023-12-21 12:46:48 -0400

    Revert "Users Services"
    This reverts commit 5408a83106f6db96a95b2da2513fb78b74a062a5.

--- GeoWeather-Server/src/config/message.js
+++
@@ -1,9 +0,0 @@
-export const message = {
-  errr: 'Error',
-  error_message: 'Error en el servidor',
-  success_long: 'Operación realizada exitosamente',
-  create_user: 'Usuario creado exitosamente',
-  create_weather: 'weather guardado exitosamente',
-  bad_request_user: 'El usuario no existe',
-  duplicate_data: 'El usuario ya se encuentra registrado'
-}
